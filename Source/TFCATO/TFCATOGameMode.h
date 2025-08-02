f72a99e1e63ff777926ee633e9357f0c6056a794
Ruslan Zaynetdinov <ruzyqwerty>
2025-08-02 10:34:33 +0300

    added UE5 FPP template files

---
+++ Source/TFCATO/TFCATOGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TFCATOGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATFCATOGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATFCATOGameMode();
+};
+
+
+
